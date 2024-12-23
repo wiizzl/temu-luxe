a312d81c4446e945c9eed006686c9aca56fdd8a2
wiizzl <wiizz76.contact@gmail.com>
2024-12-23 21:09:51 +0100

    feat: product hover effect

--- src/components/product/product-card.tsx
+++ src/components/product/product-card.tsx
@@ -12,7 +12,9 @@ type ProductCardProps = {
 const ProductCard = (props: ProductCardProps) => {
     return (
         <Link to={props.item.href} className="productCard">
-            <img src={props.item.image} alt={`Produit : ${props.item.title}`} />
+            <div className="imgContainer">
+                <img src={props.item.image} alt={`Produit : ${props.item.title}`} />
+            </div>
             <div className="content">
                 <h2>{props.item.title}</h2>
                 <p>{props.item.price} &euro;</p>
